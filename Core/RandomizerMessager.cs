07b5ae655a6775170a7ca14e5eb73932f6b6178c
c-ostic <60513574+c-ostic@users.noreply.github.com>
2024-10-26 15:34:20 -0400

    Add Color to Messages
    Adds color to messages received by archipelago.

--- Core/RandomizerMessager.cs
+++ Core/RandomizerMessager.cs
@@ -1,4 +1,6 @@
-﻿using Game;
+﻿using Archipelago.MultiClient.Net.MessageLog.Messages;
+using Archipelago.MultiClient.Net.MessageLog.Parts;
+using Game;
 using OriModding.BF.Core;
 using System;
 using System.Collections.Generic;
@@ -10,11 +12,19 @@ namespace OriBFArchipelago.Core
 {
     public class RandomizerMessager : MonoBehaviour
     {
-        public static int MAX_MESSAGES = 20;
+        // maximum number of messages before old messages are deleted so they don't flood the screen
+        public const int MAX_MESSAGES = 20;
+        // default total duration that messages last
+        public const float DEFAULT_DURATION = 8f;
+        // percentage of total duration when fade begins
+        public const float FADE_PERCENT_THRESHOLD = 0.4f;
 
         public static RandomizerMessager instance;
 
+        // controls whether messages are added to the queue
         public bool IsActive { get; private set; }
+        // controls how long messages remain on the screen
+        public float MessageDuration { get; private set; }
 
         public class RandomizerMessage
         {
@@ -29,6 +39,7 @@ namespace OriBFArchipelago.Core
                 this.currentDuration = duration;
             }
 
+            // Represents the percentage of duration left
             public float GetDurationPercent()
             {
                 return currentDuration / initDuration;
@@ -43,6 +54,7 @@ namespace OriBFArchipelago.Core
         {
             instance = this;
             IsActive = true;
+            MessageDuration = DEFAULT_DURATION;
             messageQueue = new Queue<RandomizerMessage>();
 
             messageStyle = new GUIStyle();
@@ -57,10 +69,25 @@ namespace OriBFArchipelago.Core
 
             GUILayout.BeginVertical();
 
-            foreach (var message in messageQueue)
+            foreach (RandomizerMessage message in messageQueue)
             {
-                messageStyle.normal.textColor = new Color(0.9f, 0.9f, 0.9f, message.GetDurationPercent());
-                GUILayout.Label(message.message, messageStyle);
+                float opacity = 1;
+
+                // only start to fade once the duration percent has passed the threshold
+                if (message.GetDurationPercent() < FADE_PERCENT_THRESHOLD)
+                {
+                    opacity = message.GetDurationPercent() / FADE_PERCENT_THRESHOLD;
+                }
+
+                string opacityHex = ((int)(opacity * 255)).ToString("X2");
+
+                // set default text opacity
+                messageStyle.normal.textColor = new Color(1f, 1f, 1f, opacity);
+
+                // replace any instance of ## with opacity for rich colored text
+                string formattedMessage = message.message.Replace("##", opacityHex);
+
+                GUILayout.Label(formattedMessage, messageStyle);
             }
 
             GUILayout.EndVertical();
@@ -90,17 +117,52 @@ namespace OriBFArchipelago.Core
             }
         }
 
-        public void AddMessage(string message, float duration = 5f)
+        public void AddMessage(string message)
         {
             if (IsActive)
             {
-                messageQueue.Enqueue(new RandomizerMessage(message, duration));
+                messageQueue.Enqueue(new RandomizerMessage(message, MessageDuration));
             }
+        }
+
+        public void AddMessage(LogMessage message)
+        {
+            string result = "";
+
+            foreach(MessagePart part in message.Parts)
+            {
+                // i'm unsure what messages has background color, but i'll check for it just to be safe
+                if (!part.IsBackgroundColor)
+                {
+                    // add a color tag to the text
+                    // the ## at the end is a placeholder for the opacity to be filled in at runtime
+                    result += "<color=#" + ConvertColor(part.Color) + "##>";
+                    result += part.Text;
+                    result += "</color>";
+                }
+                else
+                {
+                    result += part.Text;
+                }
+            }
+
+            AddMessage(result);
+        }
+
+        private string ConvertColor(Archipelago.MultiClient.Net.Models.Color color)
+        {
+            byte[] colorData = {color.R, color.G, color.B};
+            return BitConverter.ToString(colorData).Replace("-", string.Empty);
         }
 
         public void SetActive(bool isActive)
         {
             IsActive = isActive;
+        }
+
+        public void SetMessageDuration(float messageDuration)
+        {
+            MessageDuration = messageDuration;
         }
     }
 }
